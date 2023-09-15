c21d00ac7a2ed63ceb5c3b3e86ecbd7cf18fd917
Matteo Vidali <mvidali@iu.edu>
2023-09-15 12:26:27 -0400

    update populate to add u-dma-buf.ko

--- src/rootfs/populatefs.sh
+++ src/rootfs/populatefs.sh
@@ -3,12 +3,11 @@ sudo mkdir /mnt/uio/stuff
 sudo mkdir /mnt/uio/etc/modules
 sudo mkdir /mnt/uio/stuff/files
 sudo cp ../../compiled/uio_pcnt.ko /mnt/uio/etc/modules/uio_pcnt.ko
+sudo cp ../../compiled/u-dma-buf.ko /mnt/uio/etc/modules/u-dma-buf.ko
 sudo cp internal/inittab /mnt/uio/etc/inittab
 sudo cp internal/S80modules /mnt/uio/etc/init.d/
-sudo cp internal/S90popcount /mnt/uio/etc/init.d/
 sudo cp internal/data/* /mnt/uio/stuff/files/
 sudo chmod u+x /mnt/uio/etc/init.d/S80modules
-sudo chmod u+x /mnt/uio/etc/init.d/S90popcount
 sudo umount /mnt/uio
 sudo rmdir /mnt/uio
 echo "Finished filesystem creation"
